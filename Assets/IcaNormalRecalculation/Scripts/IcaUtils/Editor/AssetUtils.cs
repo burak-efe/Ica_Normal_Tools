c6f576e7d23f86c5e12bcae2bea99bd60bd0ce04
burak-efe <82805019+burak-efe@users.noreply.github.com>
2023-08-29 15:24:20 +0300

    Asset util find asset

--- Assets/IcaNormalRecalculation/Scripts/IcaUtils/Editor/AssetUtils.cs
+++ Assets/IcaNormalRecalculation/Scripts/IcaUtils/Editor/AssetUtils.cs
@@ -2,6 +2,13 @@
 {
     public static class AssetUtils
     {
+        public static GameObject FindAndInstantiateAsset(string name)
+        {
+            var paths = AssetDatabase.FindAssets(name);
+            var asset = AssetDatabase.LoadMainAssetAtPath(AssetDatabase.GUIDToAssetPath(paths[0]));
+            var obj = (GameObject)Object.Instantiate(asset);
+            return obj;
+        }
         
     }
 }
