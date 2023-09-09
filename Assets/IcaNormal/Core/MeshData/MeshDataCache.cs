498b2b44b1f10ef637029ba0410e0c40bbce0f72
burak-efe <82805019+burak-efe@users.noreply.github.com>
2023-09-09 15:08:36 +0300

    project auditor added

--- Assets/IcaNormal/Core/MeshData/MeshDataCache.cs
+++ Assets/IcaNormal/Core/MeshData/MeshDataCache.cs
@@ -217,45 +217,45 @@ namespace Ica.Normal
             }
         }
 
-        // try something this to remove requirement of read/ write enabled
-        public static Mesh MakeReadableMeshCopy(Mesh nonReadableMesh)
-        {
-            Mesh meshCopy = new Mesh();
-            meshCopy.indexFormat = nonReadableMesh.indexFormat;
-
-            // Handle vertices
-            GraphicsBuffer verticesBuffer = nonReadableMesh.GetVertexBuffer(0);
-            int totalSize = verticesBuffer.stride * verticesBuffer.count;
-            byte[] data = new byte[totalSize];
-            verticesBuffer.GetData(data);
-            meshCopy.SetVertexBufferParams(nonReadableMesh.vertexCount, nonReadableMesh.GetVertexAttributes());
-            meshCopy.SetVertexBufferData(data, 0, 0, totalSize);
-            verticesBuffer.Release();
-
-            // Handle triangles
-            meshCopy.subMeshCount = nonReadableMesh.subMeshCount;
-            GraphicsBuffer indexesBuffer = nonReadableMesh.GetIndexBuffer();
-            int tot = indexesBuffer.stride * indexesBuffer.count;
-            byte[] indexesData = new byte[tot];
-            indexesBuffer.GetData(indexesData);
-            meshCopy.SetIndexBufferParams(indexesBuffer.count, nonReadableMesh.indexFormat);
-            meshCopy.SetIndexBufferData(indexesData, 0, 0, tot);
-            indexesBuffer.Release();
-
-            // Restore submesh structure
-            uint currentIndexOffset = 0;
-            for (int i = 0; i < meshCopy.subMeshCount; i++)
-            {
-                uint subMeshIndexCount = nonReadableMesh.GetIndexCount(i);
-                meshCopy.SetSubMesh(i, new SubMeshDescriptor((int)currentIndexOffset, (int)subMeshIndexCount));
-                currentIndexOffset += subMeshIndexCount;
-            }
-
-            // Recalculate normals and bounds
-            meshCopy.RecalculateNormals();
-            meshCopy.RecalculateBounds();
-
-            return meshCopy;
-        }
+        // // try something this to remove requirement of read/ write enabled
+        // public static Mesh MakeReadableMeshCopy(Mesh nonReadableMesh)
+        // {
+        //     Mesh meshCopy = new Mesh();
+        //     meshCopy.indexFormat = nonReadableMesh.indexFormat;
+        //
+        //     // Handle vertices
+        //     GraphicsBuffer verticesBuffer = nonReadableMesh.GetVertexBuffer(0);
+        //     int totalSize = verticesBuffer.stride * verticesBuffer.count;
+        //     byte[] data = new byte[totalSize];
+        //     verticesBuffer.GetData(data);
+        //     meshCopy.SetVertexBufferParams(nonReadableMesh.vertexCount, nonReadableMesh.GetVertexAttributes());
+        //     meshCopy.SetVertexBufferData(data, 0, 0, totalSize);
+        //     verticesBuffer.Release();
+        //
+        //     // Handle triangles
+        //     meshCopy.subMeshCount = nonReadableMesh.subMeshCount;
+        //     GraphicsBuffer indexesBuffer = nonReadableMesh.GetIndexBuffer();
+        //     int tot = indexesBuffer.stride * indexesBuffer.count;
+        //     byte[] indexesData = new byte[tot];
+        //     indexesBuffer.GetData(indexesData);
+        //     meshCopy.SetIndexBufferParams(indexesBuffer.count, nonReadableMesh.indexFormat);
+        //     meshCopy.SetIndexBufferData(indexesData, 0, 0, tot);
+        //     indexesBuffer.Release();
+        //
+        //     // Restore submesh structure
+        //     uint currentIndexOffset = 0;
+        //     for (int i = 0; i < meshCopy.subMeshCount; i++)
+        //     {
+        //         uint subMeshIndexCount = nonReadableMesh.GetIndexCount(i);
+        //         meshCopy.SetSubMesh(i, new SubMeshDescriptor((int)currentIndexOffset, (int)subMeshIndexCount));
+        //         currentIndexOffset += subMeshIndexCount;
+        //     }
+        //
+        //     // Recalculate normals and bounds
+        //     meshCopy.RecalculateNormals();
+        //     meshCopy.RecalculateBounds();
+        //
+        //     return meshCopy;
+        // }
     }
 }
