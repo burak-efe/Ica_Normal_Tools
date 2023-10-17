c09cef1cf7f9b90cce3864daa6108cf22b0be80e
burak-efe <82805019+burak-efe@users.noreply.github.com>
2023-10-17 03:24:26 +0300

    add attribution to tangent calculation

--- Assets/Plugins/IcaNormal/Runtime/Calculation/Jobs/TangentJobs.cs
+++ Assets/Plugins/IcaNormal/Runtime/Calculation/Jobs/TangentJobs.cs
@@ -8,6 +8,8 @@ namespace Ica.Normal
 {
     public class TangentJobs
     {
+        //taken from
+        //https://www.cs.upc.edu/~virtual/G/1.%20Teoria/06.%20Textures/Tangent%20Space%20Calculation.pdf
         [BurstCompile]
         public struct TriTangentJob : IJobFor
         {
